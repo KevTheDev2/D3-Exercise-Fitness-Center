44c1181f79b8a05f7efd7ea5fc4fd3b1aabee7b6
Kevin Gibbs II <kgibbs136@gmail.com>
2025-05-21 20:43:38 -0400

    5/21.2

---
+++ FitnessProjectD3/FitnessProjectD3/SingleClub.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessProjectD3
+{
+    
+    class SingleClub : Member
+    {
+        SingleClub singleClub1 = new SingleClub(true);
+
+        public bool singleClub = true;
+
+    public SingleClub()
+        { 
+        
+        }
+    }
+}
