4ad5b8af6a5dd22314fb4167915fde144a22ade4
Marquise Adeleye <marquiseadeleye@gmail.com>
2025-05-22 19:38:06 -0400

    Program

--- FitnessProjectD3/FitnessProjectD3/Program.cs
+++ FitnessProjectD3/FitnessProjectD3/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine(---------------------------"Start Program------------------------");
+        
+        //Create list of clubs in program or have the list exist in the club class
+        List<Club> clubList = new List<Club>();
+        clubList.Add(new Club("Club 1", "1234567890"));
+        clubList.Add(new Club("Club 1", "1234567890"));
+        clubList.Add(new Club("Club 1", "1234567890"));
+        clubList.Add(new Club("Club 1", "1234567890"));
+        //Show list of clubs
+        
+        
+        //Ask the user if they want to select a club
+        
+        
+        //Ask the user if they want to chekin
+        
+        
+        //Ask user if they want to select a club
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        Console.WriteLine(---------------------------"End Program--------------------------");
     }
 }
