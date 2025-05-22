1a4099405f88bd624e2df66bdae6ab76c170ad61
Marquise Adeleye <marquiseadeleye@gmail.com>
2025-05-21 20:40:34 -0400

    Members update

--- FitnessProjectD3/FitnessProjectD3/Member.cs
+++ FitnessProjectD3/FitnessProjectD3/Member.cs
@@ -4,12 +4,7 @@ public abstract class Member
 {
     private int _id { get; set; }
     private string _name { get; set; }
-
-    Member(string name)
-    {
-        _id = _id.GetHashCode();
-        _name = name;
-    }
+    
     
   //  public abstract void CheckIn(Club club);
 }
